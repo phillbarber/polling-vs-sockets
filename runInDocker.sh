f43b08fb1d717ab1130b3100fd7a68e7e7628396
Phill <Phill>
2018-05-14 21:31:04 +0100

    Add startup bash script

--- runInDocker.sh
+++ runInDocker.sh
@@ -6,4 +6,4 @@ SHORT_NAME=polling-vs-websockets
 
 FULL_IMAGE_NAME="$USER_NAME/$SHORT_NAME";
 
-docker run --kernel-memory=1024m --cpus=1 -d -p 8080:8080 -p 8081:8081 $FULL_IMAGE_NAME java -jar /server.jar
+docker run --kernel-memory=1024m --cpus=1 -d -p 8080:8080 -p 8081:8081 $FULL_IMAGE_NAME /startServerInDocker.sh
