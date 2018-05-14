2be020409eada8ca4345b3e01a78bd054d5cf8fd
Phill <Phill>
2018-05-14 21:58:05 +0100

    Set threads to 100

--- performancetests/runPollingPerfTest.sh
+++ performancetests/runPollingPerfTest.sh
@@ -4,4 +4,4 @@ set -e;
 rm target/log || true
 rm -rf target/report || true
 
-/usr/share/apache-jmeter-4.0/bin/jmeter -e -o target/report -l target/log -n -Jthreads=99 -JjobsPerThread=2 -JpollingDelayMs=100  -t http-polling.jmx
+/usr/share/apache-jmeter-4.0/bin/jmeter -e -o target/report -l target/log -n -Jthreads=100 -JjobsPerThread=2 -JpollingDelayMs=100  -t http-polling.jmx
