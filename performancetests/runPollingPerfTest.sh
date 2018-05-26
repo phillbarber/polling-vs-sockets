fff97008bfd86dc5ba672e30aa86688fa152ce75
Phill <Phill>
2018-05-26 22:37:13 +0100

    Add directory hierarchy to reports

--- performancetests/runPollingPerfTest.sh
+++ performancetests/runPollingPerfTest.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e;
 
-rm target/log || true
-rm -rf target/report || true
+DATE_TIME="$(date --iso-8601=seconds)"
+REPORT_DIR="target/report/polling/$DATE_TIME"
+REPORT_HTML_DIR="${REPORT_DIR}/html"
 
-/usr/share/apache-jmeter-4.0/bin/jmeter -e -o target/report -l target/log -n -Jthreads=100 -JjobsPerThread=10 -JmaxDurationOfJobToComplete=100 -JpollingDelayMs=100  -t http-polling.jmx
+mkdir -p ${REPORT_DIR}
+mkdir -p ${REPORT_HTML_DIR}
+
+COMMAND="/usr/share/apache-jmeter-4.0/bin/jmeter -e -o ${REPORT_HTML_DIR} -l ${REPORT_DIR}/log -n -Jthreads=10 -JjobsPerThread=3 -JmaxDurationOfJobToComplete=10500 -JpollingDelayMs=500  -t http-polling.jmx"
+
+echo "${COMMAND}" > ${REPORT_DIR}/command.txt
+
+echo "${COMMAND}"
+
+eval ${COMMAND}
