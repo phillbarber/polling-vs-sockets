7bf1148dfa8712855a66fd72c0272bb372aa0a35
Phill <Phill>
2018-05-31 17:25:25 +0100

    Tidied performance test variables.

--- performancetests/runPollingPerfTest.sh
+++ performancetests/runPollingPerfTest.sh
@@ -4,11 +4,12 @@ set -e;
 DATE_TIME="$(date --iso-8601=seconds)"
 REPORT_DIR="target/report/polling/$DATE_TIME"
 REPORT_HTML_DIR="${REPORT_DIR}/html"
+export JVM_ARGS="-Xms512m -Xmx512m"
 
 mkdir -p ${REPORT_DIR}
 mkdir -p ${REPORT_HTML_DIR}
 
-COMMAND="/usr/share/apache-jmeter-4.0/bin/jmeter -e -o ${REPORT_HTML_DIR} -l ${REPORT_DIR}/log -n -Jthreads=10 -JjobsPerThread=3 -JmaxDurationOfJobToComplete=10500 -JpollingDelayMs=500  -t http-polling.jmx"
+COMMAND="/usr/share/apache-jmeter-4.0/bin/jmeter -e -o ${REPORT_HTML_DIR} -l ${REPORT_DIR}/log -n -Jthreads=200 -JjobsPerThread=10 -JmaxDurationOfJobToCompleteMS=11000 -JpollingDelayMS=500  -t http-polling.jmx"
 
 echo "${COMMAND}" > ${REPORT_DIR}/command.txt
 
--- performancetests/runWebSocketsPerfTest.sh
+++ performancetests/runWebSocketsPerfTest.sh
@@ -4,11 +4,12 @@ set -e;
 DATE_TIME="$(date --iso-8601=seconds)"
 REPORT_DIR="target/report/sockets/$DATE_TIME"
 REPORT_HTML_DIR="${REPORT_DIR}/html"
+export JVM_ARGS="-Xms512m -Xmx1024m"
 
 mkdir -p ${REPORT_DIR}
 mkdir -p ${REPORT_HTML_DIR}
 
-COMMAND="/usr/share/apache-jmeter-4.0/bin/jmeter -e -o ${REPORT_HTML_DIR} -l ${REPORT_DIR}/log -n -Jthreads=200 -JjobsPerThread=10 -JmaxDurationOfJobToComplete=10100 -t websockets.jmx"
+COMMAND="/usr/share/apache-jmeter-4.0/bin/jmeter -e -o ${REPORT_HTML_DIR} -l ${REPORT_DIR}/log -n -Jthreads=200 -JjobsPerThread=10 -JmaxDurationOfJobToCompleteMS=11000 -t websockets.jmx"
 
 echo "${COMMAND}" > ${REPORT_DIR}/command.txt
 
