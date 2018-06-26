4158194fb7f9bb9353a78dac8cd92791e2705893
Phill <Phill>
2018-06-26 21:44:47 +0100

    Add create time to Job

--- src/main/java/com/github/phillbarber/job/Job.kt
+++ src/main/java/com/github/phillbarber/job/Job.kt
@@ -1,5 +1,9 @@
 package com.github.phillbarber.job
 
+
+import java.time.Instant
 import java.util.*
 
-data class Job (val id: String = UUID.randomUUID().toString(), var complete: Boolean = false)
+data class Job (val id: String = UUID.randomUUID().toString(),
+                val createTime: Long = Instant.now().toEpochMilli(),
+                var complete: Boolean = false)
